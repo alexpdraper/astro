c52410d2e3406c1f9ae3d6a51239007d36f750d5
Nate Moore <nate@astro.build>
2022-11-08 11:57:43 -0600

    fix: appease typescript

--- packages/astro/src/jsx/babel.ts
+++ packages/astro/src/jsx/babel.ts
@@ -143,6 +143,7 @@ export default function astroJSX(): PluginObj {
 							clientOnlyComponents: [],
 							hydratedComponents: [],
 							scripts: [],
+							pageOptions: {}
 						};
 					}
 					path.node.body.splice(
